389a1f8dd8ff4579ac993e9ec40a58c17df94776
Nathaniel Katz <Nathaniel Katz>
2017-03-01 23:09:06 +0200

    fixed on-click to pull from array and cleaned up code

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-
-var sampleHebrewWords = [
-  {
-    hebrew_word: "דג",
-    english_word: "fish",
+var sampleHebrewWords = [{
+    Hebrew: "דג",
+    English: "fish",
     point_value: 0
   },
   {
-    hebrew_word: "כלב",
-    english_word: "dog",
+    Hebrew: "כלב",
+    English: "dog",
     point_value: 0
   },
   {
-    hebrew_word: "חתול",
-    english_word: "cat",
+    Hebrew: "חתול",
+    English: "cat",
     point_value: 0
   }
 ]
 
-$("#cycle button").click(function(){
 
-  function logContent(el, index, array) {
-    console.log('a[' + index + '] = ' + el.hebrew_word);
-  };
-  sampleHebrewWords.forEach(logContent);
+$("#cycle button").click(function() {
 
+  // Reset the DOM on click
+  $(".card").remove();
 
+  // create divs and append to dom
+  $.each(sampleHebrewWords, function(index, value) {
 
-  $(".flashcard").fadeOut(1000)
-    .text(sampleHebrewWords[0].hebrew_word).fadeOut(1000);
+    var heb = "<div class='card'>" + sampleHebrewWords[index].Hebrew + "</div>";
+    var en = "<div class='card'>" + sampleHebrewWords[index].English + "</div>";
 
+    $("#magic").append(heb).append(en);
 
-});
+  });
+
+  // add unique IDs and fade in
+  $('.card').each(function(index) {
+    $(this).attr('id', 'card' + index);
+    $('#card' + (index)).delay(1000 * index).fadeIn().fadeOut();
+  });
+
+}); // end of on-click
